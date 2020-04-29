205c1a8a099739530945fb8461abec438d651e6f
latinawhocodes <alyssaroque01@gmail.com>
2020-04-28 23:03:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
-// Write your code here
+//Write Your Code Here
 const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
-const random = Math.floor( Math.random() * 100 ) + 1
 
-const mod = 20 % 8;
+const random = Math.random();
 
-const max = 20;
+const mod = 24 % 5;
+
+const max = Math.max(12, 14, 7, 2, 18, 20, 3, 4, 0);
